2559a41b438ee55742908f6665aa6c0239c9fd53
Thanh Pham <khanhthanhh9@gmail.com>
2025-02-06 07:59:11 +0700

    add sending message functionality

--- lib/screens/chatroom_screen.dart
+++ lib/screens/chatroom_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:globalchat/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
 class ChatRoomScreen extends StatefulWidget {
   String chatroomName;
@@ -11,6 +14,26 @@ class ChatRoomScreen extends StatefulWidget {
 }
 
 class _ChatRoomScreenState extends State<ChatRoomScreen> {
+  TextEditingController messageText = TextEditingController();
+
+  var db = FirebaseFirestore.instance;
+
+  Future<void> sendMessage() async {
+    if (messageText.text.isEmpty) {
+      return;
+    }
+    Map<String, dynamic> messageToSend = {
+      "text": messageText.text,
+      "sender_name": Provider.of<UserProvider>(context, listen: false).userName,
+      "chatroom_id": widget.chatroomId,
+      "timestamp": FieldValue.serverTimestamp()
+    };
+    try {
+      await db.collection("messages").add(messageToSend);
+      messageText.text = " ";
+    } catch (e) {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,13 +42,27 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
         children: [
           Expanded(
             child: Container(
-              color: Colors.red,
+              color: Colors.white,
             ),
           ),
-          Expanded(
-              child: Row(
-            children: [Expanded(child: TextField()), Icon(Icons.send)],
-          ))
+          Container(
+            color: Colors.grey[200],
+            child: Row(
+              children: [
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: TextField(
+                    controller: messageText,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter Message Here ... "),
+                  ),
+                )),
+                InkWell(onTap: sendMessage, child: Icon(Icons.send))
+              ],
+            ),
+          )
         ],
       ),
     );
---
+++ q
@@ -0,0 +1,17 @@
+  CP1[m
+  CP10[m
+  CP11[m
+  CP12[m
+  CP13[m
+  CP14[m
+  CP15[m
+* [32mCP16[m
+  CP2[m
+  CP3[m
+  CP4[m
+  CP5[m
+  CP6[m
+  CP7[m
+  CP8[m
+  CP9[m
+  master[m
